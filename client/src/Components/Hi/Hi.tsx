085a53687d1d165d06c06c00f1afc7fd90958387
Simon Podlipsky <simon@podlipsky.net>
2023-01-24 16:55:00 +0100

    repro

---
+++ client/src/Components/Hi/Hi.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Hi: React.FC = () => {
+  return <>hi</>;
+};
---
+++ client/src/Pages/Hi/routes/Hi.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import { Hi as HiComponent } from '@app/client/Components/Hi/Hi';
+
+const Hi: React.FC = () => <HiComponent />;
+
+export default Hi;
